1e269fee1ee2a519886d3caf7619bbc23669d195
Matthias Bilger <matthias@bilger.info>
2016-11-22 11:08:04 +0100

    Added font and debug possibilities

--- setup.py
+++ setup.py
@@ -13,20 +13,21 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name = 'yafte',
-    version = '0.6.0',
+    version = '0.7.0',
     description = 'Using yaml for fpdf templates',
     long_description = long_description,
     url = 'https://github.com/m42e/yamlfpdftemplate',
+    download_url = 'https://github.com/m42e/yamlfpdftemplate/archive/v0.7.0.tar.gz',
     author = 'Matthias Bilger',
     author_email = 'matthias@bilger.info',
     license = 'MIT',
     classifiers=[
-		'Topic :: Text Processing',
+        'Topic :: Text Processing',
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
-	],
-	keywords = 'template yaml fpdf',
-	packages = find_packages(exclude=['example']),
-	install_requires = ['pyyaml', 'fpdf'],
+    ],
+    keywords = 'template yaml fpdf',
+    packages = find_packages(exclude=['example']),
+    install_requires = ['pyyaml', 'fpdf'],
 )
